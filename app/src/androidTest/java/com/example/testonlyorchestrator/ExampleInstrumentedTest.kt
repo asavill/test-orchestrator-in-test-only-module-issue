61120dc402c502339830e930b5ba5a547f4846b4
asavill <alexsavill@me.com>
2020-10-16 10:23:45 +0100

    Sample project to demonstrate androidx.test:orchestrator in test-only module not working.

---
+++ app/src/androidTest/java/com/example/testonlyorchestrator/ExampleInstrumentedTest.kt
@@ -0,0 +1,46 @@
+package com.example.testonlyorchestrator
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    var rule: ActivityScenarioRule<MainActivity> =
+        ActivityScenarioRule<MainActivity>(MainActivity::class.java)
+
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.testonlyorchestrator", appContext.packageName)
+    }
+
+    @Test
+    fun checkForHelloWorld() {
+        Espresso.onView(ViewMatchers.withText("Hello World!")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun checkForHelloAndroidTestTeam() {
+        Espresso.onView(ViewMatchers.withText("Hello Android Test Team"))
+            .check(matches(isDisplayed()))
+    }
+}
